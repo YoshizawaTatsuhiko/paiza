b1c0bcca8d7352b7f098bfd0f4eeab07da1eb391
tatsupiko <identity2022@outlook.jp>
2023-04-19 14:02:30 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var data = Console.ReadLine();
+        Console.WriteLine(data.Trim('F'));
+    }
+}
---
+++ test0/Template.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Template
+{
+    void Deligate()
+    {
+        // 配列に格納された整数を降順で並べ替える
+        int[] arr = { 10, 409, 3, 64, 0, -2, 13 };
+        //Array.Sort(arr, (a, b) => b - a);
+        Array.Sort(arr, (int x, int y) => { return y - x; });
+        //Array.ForEach(arr, Console.WriteLine);
+        Array.ForEach(arr, (int i) => { Console.WriteLine(i); });
+    }
+}
