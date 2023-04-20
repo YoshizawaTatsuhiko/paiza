12590e81b0101e2d7bbdca8d8ead71b14d6d75eb
tatsupiko <identity2022@outlook.jp>
2023-04-21 00:22:23 +0900

    QueueクラスでDequeue()関数を呼び出したときにエラーがスローされないように処理を組んだ。

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -6,6 +6,32 @@ class Program
 {
     static void Main()
     {
-        
+        Queue<int> queue = new Queue<int>();
+        int count = 0;
+        int data = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < data; i++)
+        {
+            string[] tmp = Console.ReadLine().Split();
+
+            if (tmp[0] == "1")
+            {
+                queue.Enqueue(int.Parse(tmp[1]));
+                count++;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        Console.WriteLine(count);
+
+        for (int i = 0; i < data; i++)
+        {
+            if (queue.Count != 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
     }
 }
