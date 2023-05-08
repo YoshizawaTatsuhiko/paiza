ee4ea9274d34f8d73bf03081a3d7a3c8a6ef385e
tatsupiko <identity2022@outlook.jp>
2023-05-08 21:36:53 +0900

    素数メニュー | Paiza予想 | 中国剰余定理

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -4,40 +4,17 @@ class Program
 {
     static void Main()
     {
-        int data = int.Parse(Console.ReadLine());
-        double[] memoNum = new double[2];
+        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        double m1 = data[0], m2 = data[1];
+        int b1 = data[2], b2 = data[3];
+        double ans = 0;
 
-        for (int i = 2; i <= data / 2; i++)
+        for (double i = 0; i < m1 * m2; i++)
         {
-            double diff = data - i;
+            ans = m1 * i + b1;
 
-            if (!IsPrime(i)) continue;
-            else if (IsPrime(diff))
-            {
-                if (memoNum[0] * memoNum[1] < i * diff)
-                {
-                    memoNum[0] = i;
-                    memoNum[1] = diff;
-                }
-            }
+            if (ans % m2 == b2) break;
         }
-
-        for (int i = 0; i < memoNum.Length; i++)
-        {
-            Console.WriteLine(memoNum[i]);
-        }
-    }
-
-    static bool IsPrime(double n)
-    {
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
-
-        for (int i = 3; i * i <= n; i += 2)
-        {
-            if (n % i == 0) return false;
-        }
-        return true;
+        Console.WriteLine(ans);
     }
 }
