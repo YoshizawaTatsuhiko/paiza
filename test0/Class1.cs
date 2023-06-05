10073a88ca59b49a85952671d20622cbc7ed4a46
tatsupiko <identity2022@outlook.jp>
2023-06-05 15:11:33 +0900

    グラフ・DFSメニュー | パスの通れない頂点２ | STEP１

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -6,22 +6,23 @@ class Program
     static void Main()
     {
         int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        int[] intArray = new int[data[2] + 1];
-        List<int> vertexNum = new List<int>();
-        Random random = new Random();
 
         for (int i = 1; i <= data[0]; i++)
         {
-            if (i == data[1]) intArray[0] = i;
-            else vertexNum.Add(i);
-        }
+            Console.ReadLine();
 
-        for (int i = 1; i <= data[2]; i++)
-        {
-            int n = random.Next(vertexNum.Count);
-            intArray[i] = vertexNum[n];
-            vertexNum.RemoveAt(n);
+            if (i == data[1])
+            {
+                int[] vertices = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                Array.Sort(vertices);
+
+                if (vertices.Length > 0) Console.WriteLine(string.Join(" ", vertices));
+                else Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.ReadLine();
+            }
         }
-        Console.WriteLine(string.Join(" ", intArray));
     }
 }
