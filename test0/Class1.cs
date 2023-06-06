13102ba18fc74d0c0626d1fa736332e0ec557fed
tatsupiko <identity2022@outlook.jp>
2023-06-07 01:43:52 +0900

    グラフ・DFSメニュー | パスの通れない頂点２ | FINAL

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -12,12 +12,12 @@ class Program
         list.Add(data[1]);
         int wayCount = 0;
 
-        for (int i = 1; i <= data[0]; i++)
+        for (int i = 0; i <= data[0]; i++)
         {
             Console.ReadLine();
             vertexInfo.Add(i, Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
         }
-        SearchRuts(data[1], data[2], data[3], list, vertexInfo, result, ref wayCount);
+        SearchRuts(data[1], data[2], list, vertexInfo, result, ref wayCount);
         Console.WriteLine(wayCount);
 
         for (int i = 0; i < result.Count; i++)
@@ -26,12 +26,22 @@ class Program
         }
     }
 
-    static void SearchRuts(int startVertex, int endVertex, int waypoint, List<int> list, Dictionary<int, int[]> vertexInfo, List<string> result, ref int wayCount)
+    static void SearchRuts(int startVertex, int endVertex, List<int> list, Dictionary<int, int[]> vertexInfo, List<string> result, ref int wayCount)
     {
-        if (endVertex == startVertex && list.Contains(waypoint))
+        if (endVertex == startVertex)
         {
-            result.Add(string.Join(" ", list));
-            wayCount++;
+            bool hasNotPassedPoint = true;
+
+            for (int i = 0; i < vertexInfo[0].Length; i++)
+            {
+                if (list.Contains(vertexInfo[0][i])) hasNotPassedPoint = false;
+            }
+
+            if (hasNotPassedPoint)
+            {
+                result.Add(string.Join(" ", list));
+                wayCount++;
+            }
         }
         else
         {
@@ -42,7 +52,7 @@ class Program
                 if (!list.Contains(nextVertex))
                 {
                     list.Add(nextVertex);
-                    SearchRuts(nextVertex, endVertex, waypoint, list, vertexInfo, result, ref wayCount);
+                    SearchRuts(nextVertex, endVertex, list, vertexInfo, result, ref wayCount);
                     list.RemoveAt(list.Count - 1);
                 }
             }
