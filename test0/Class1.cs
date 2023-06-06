ad79385b99738e6448920c607773f76f012e545c
tatsupiko <identity2022@outlook.jp>
2023-06-06 23:55:51 +0900

    グラフ・DFSメニュー | パスの通れない頂点２ | STEP２

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -6,22 +6,42 @@ class Program
     static void Main()
     {
         int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        Dictionary<int, int[]> vertexInfo = new Dictionary<int, int[]>();
+        List<string> result = new List<string>();
+        List<int> list = new List<int>();
+        list.Add(data[1]);
+        int wayCount = 0;
 
         for (int i = 1; i <= data[0]; i++)
         {
             Console.ReadLine();
+            vertexInfo.Add(i, Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+        }
+        SearchRuts(data[1], list, data[2], vertexInfo, result, ref wayCount);
+        Console.WriteLine(wayCount);
 
-            if (i == data[1])
-            {
-                int[] vertices = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-                Array.Sort(vertices);
+        for (int i = 0; i < result.Count; i++)
+        {
+            Console.WriteLine(result[i]);
+        }
+    }
 
-                if (vertices.Length > 0) Console.WriteLine(string.Join(" ", vertices));
-                else Console.WriteLine(-1);
-            }
-            else
+    static void SearchRuts(int vertex, List<int> list,
+        int walkCount, Dictionary<int, int[]> vertexInfo, List<string> result, ref int wayCount)
+    {
+        if (walkCount <= 0)
+        {
+            result.Add(string.Join(" ", list));
+            wayCount++;
+        }
+        else
+        {
+            for (int i = 0; i < vertexInfo[vertex].Length; i++)
             {
-                Console.ReadLine();
+                int nextVertex = vertexInfo[vertex][i];
+                list.Add(nextVertex);
+                SearchRuts(nextVertex, list, walkCount - 1, vertexInfo, result, ref wayCount);
+                list.RemoveAt(list.Count - 1);
             }
         }
     }
