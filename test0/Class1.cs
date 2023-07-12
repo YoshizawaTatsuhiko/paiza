8da6b2e4f5595bdce03dfa298086d6b3184e430a
tatsupiko <identity2022@outlook.jp>
2023-07-12 17:53:14 +0900

    Aランクレベルアップメニュー | 連結の判定 | STEP２

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -1,26 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Program
 {
     static void Main()
     {
         int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        int[,] adjacencyMatrix = new int[data[0], data[0]];
+        Dictionary<int, List<int>> adjacencyList = new Dictionary<int, List<int>>();
+
+        for (int i = 0; i < data[0]; i++)
+        {
+            adjacencyList.Add(i, new List<int>());
+        }
 
         for (int i = 0; i < data[1]; i++)
         {
             int[] info = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            adjacencyMatrix[info[0] - 1, info[1] - 1] = 1;
-            adjacencyMatrix[info[1] - 1, info[0] - 1] = 1;
+            adjacencyList[info[0] - 1].Add(info[1] - 1);
+            adjacencyList[info[1] - 1].Add(info[0] - 1);
         }
 
-        for (int i = 0; i < adjacencyMatrix.GetLength(0); i++)
+        for (int i = 0; i < data[0]; i++)
         {
-            for (int j = 0; j < adjacencyMatrix.GetLength(1); j++)
+            if (adjacencyList[i].Count > 0)
             {
-                Console.Write(adjacencyMatrix[i, j]);
+                adjacencyList[i].Sort();
             }
-            Console.Write("\n");
+
+            foreach (var n in adjacencyList[i])
+            {
+                Console.Write(n);
+            }
+            Console.Write('\n');
         }
     }
 }
