0ee142bc1b8fffbded8289e7487d6d834385dffc
tatsupiko <identity2022@outlook.jp>
2023-07-12 17:53:49 +0900

    Aランクレベルアップメニュー | 連結の判定 | STEP３

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -6,6 +6,7 @@ public class Program
     static void Main()
     {
         int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int[,] adjacencyMatrix = new int[data[0], data[0]];
         Dictionary<int, List<int>> adjacencyList = new Dictionary<int, List<int>>();
 
         for (int i = 0; i < data[0]; i++)
@@ -16,8 +17,17 @@ public class Program
         for (int i = 0; i < data[1]; i++)
         {
             int[] info = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            adjacencyMatrix[info[0] - 1, info[1] - 1] = 1;
             adjacencyList[info[0] - 1].Add(info[1] - 1);
-            adjacencyList[info[1] - 1].Add(info[0] - 1);
+        }
+
+        for (int i = 0; i < data[0]; i++)
+        {
+            for (int j = 0; j < data[0]; j++)
+            {
+                Console.Write(adjacencyMatrix[i, j]);
+            }
+            Console.Write('\n');
         }
 
         for (int i = 0; i < data[0]; i++)
