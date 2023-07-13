a8fb3a50e57885b08577214638158e9d8fc324b1
tatsupiko <identity2022@outlook.jp>
2023-07-13 16:26:28 +0900

    paizaの森練習問題コンテスト過去問題7 | 気温２

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -1,41 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 public class Program
 {
     static void Main()
     {
-        int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
-        HashSet<int> visited = new HashSet<int>();
-        bool[] stampCard = new bool[nm[0]];
+        Console.ReadLine();
+        int[] temperatureArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int min = int.MaxValue;
+        int max = int.MinValue;
 
-        for (int i = 0; i < nm[1]; i++)
+        for (int i = 0; i < temperatureArray.Length; i++)
         {
-            int[] datas = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            edges.Add(new Tuple<int, int>(datas[0], datas[1]));
+            if (min > temperatureArray[i]) min = temperatureArray[i];
+            if (max < temperatureArray[i]) max = temperatureArray[i];
         }
-        BFS(1, edges, visited, stampCard);
-
-        Console.WriteLine(stampCard.All(a => a == true) ? "YES" : "NO");
-    }
-
-    static void BFS(int current, List<Tuple<int, int>> edges, HashSet<int> visited, bool[] stampCard)
-    {
-        visited.Add(current);
-        stampCard[current - 1] = true;
-
-        foreach (var edge in edges)
-        {
-            if (current == edge.Item1 && !visited.Contains(edge.Item2))
-            {
-                BFS(edge.Item2, edges, visited, stampCard);
-            }
-            else if(current == edge.Item2 && !visited.Contains(edge.Item1))
-            {
-                BFS(edge.Item1, edges, visited, stampCard);
-            }
-        }
+        Console.WriteLine(max - min);
     }
 }
