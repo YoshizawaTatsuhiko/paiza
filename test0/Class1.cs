86226d3fcbd71b79e9c94c76af9593fac9d9a234
tatsupiko <identity2022@outlook.jp>
2023-07-13 16:31:42 +0900

    paizaの森練習問題コンテスト過去問題7 | ブロックの埋め込み

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -4,16 +4,9 @@ public class Program
 {
     static void Main()
     {
-        Console.ReadLine();
-        int[] temperatureArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        int min = int.MaxValue;
-        int max = int.MinValue;
-
-        for (int i = 0; i < temperatureArray.Length; i++)
-        {
-            if (min > temperatureArray[i]) min = temperatureArray[i];
-            if (max < temperatureArray[i]) max = temperatureArray[i];
-        }
-        Console.WriteLine(max - min);
+        int[] boardSize = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int height = boardSize[0];
+        int width = boardSize[1];
+        Console.WriteLine(height * width / 2);
     }
 }
