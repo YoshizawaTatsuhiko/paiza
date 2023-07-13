97da42e427a7d36dafdccf768f6a948b50c3d738
tatsupiko <identity2022@outlook.jp>
2023-07-13 16:19:36 +0900

    Aランクレベルアップメニュー | 連結の判定 | FINAL

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -1,40 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
+        int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
         List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
         HashSet<int> visited = new HashSet<int>();
+        bool[] stampCard = new bool[nm[0]];
 
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 0; i < nm[1]; i++)
         {
             int[] datas = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
             edges.Add(new Tuple<int, int>(datas[0], datas[1]));
         }
-        BFS(1, edges, visited);
+        BFS(1, edges, visited, stampCard);
 
-        foreach (var trail in visited)
-        {
-            Console.WriteLine(trail);
-        }
+        Console.WriteLine(stampCard.All(a => a == true) ? "YES" : "NO");
     }
 
-    static void BFS(int current, List<Tuple<int, int>> edges, HashSet<int> visited)
+    static void BFS(int current, List<Tuple<int, int>> edges, HashSet<int> visited, bool[] stampCard)
     {
         visited.Add(current);
+        stampCard[current - 1] = true;
 
         foreach (var edge in edges)
         {
             if (current == edge.Item1 && !visited.Contains(edge.Item2))
             {
-                BFS(edge.Item2, edges, visited);
+                BFS(edge.Item2, edges, visited, stampCard);
             }
             else if(current == edge.Item2 && !visited.Contains(edge.Item1))
             {
-                BFS(edge.Item1, edges, visited);
+                BFS(edge.Item1, edges, visited, stampCard);
             }
         }
     }
