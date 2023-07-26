9e16e52ef783d5261a932cb27acacd4c348fdcff
tatsupiko <identity2022@outlook.jp>
2023-07-26 19:54:16 +0900

    幅優先探索・深さ優先探索メニュー | 2 頂点間の最短経路 | STEP１

--- test0/Class1.cs
+++ test0/Class1.cs
@@ -1,27 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Program
+class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
-        int max = int.MinValue;
+        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int n = data[0];
+        int k = data[1];
+        List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
 
-        for (int i = 1; i < n; i++)
+        for (int i = 0; i < n - 1; i++)
         {
-            for (int j = i + 1; j <= n; j++)
-            {
-                int d = GCD(i, j);
-
-                if(max < d) max = d;
-            }
+            int[] edge = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            edges.Add(new Tuple<int, int>(edge[0], edge[1]));
         }
-        Console.WriteLine(max);
+        data = SearchVertex(k, edges);
+        Array.Sort(data);
+
+        foreach (var d in data)
+        {
+            Console.WriteLine(d);
+        }
     }
 
-    static int GCD(int a, int b)
+    static int[] SearchVertex(int vertex, List<Tuple<int, int>> edges)
     {
-        if (b == 0) return a;
-        return GCD(b, a % b);
+        List<int> adjacencyVertex = new List<int>();
+
+        foreach (var edge in edges)
+        {
+            if (edge.Item1 == vertex) adjacencyVertex.Add(edge.Item2);
+            else if(edge.Item2 == vertex) adjacencyVertex.Add(edge.Item1);
+        }
+        return adjacencyVertex.ToArray();
     }
 }
