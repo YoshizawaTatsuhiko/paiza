4b33e552b36be9f91af54bc279cc85271303c50a
tatsupiko <identity2022@outlook.jp>
2023-10-06 15:44:32 +0900

    paizaの森練習問題コンテスト過去問題8 | 乗客人数 - その 2

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,24 +4,17 @@ class Program
 {
     static void Main()
     {
-        int[] intArray = new int[40 + 1]; // 入力される値の最大値 + 1
-        intArray[0] = 0;
-        intArray[1] = 1;
-        int[] indeces = new int[int.Parse(Console.ReadLine())];
+        Console.ReadLine();
+        int[] getOffPassengers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int[] getOnPassengers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int maxPassenger = 0;
 
-        for (int i = 0; i < indeces.Length; i++)
+        for (int i = 0, count = 0; i < getOnPassengers.Length; i++)
         {
-            indeces[i] = int.Parse(Console.ReadLine());
-        }
+            count += -getOffPassengers[i] + getOnPassengers[i];
 
-        for (int i = 2; i < intArray.Length; i++)
-        {
-            intArray[i] = intArray[i - 2] + intArray[i - 1];
+            if (count > maxPassenger) maxPassenger = count;
         }
-
-        foreach (var index in indeces)
-        {
-            Console.WriteLine(intArray[index]);
-        }
+        Console.WriteLine(maxPassenger);
     }
 }
