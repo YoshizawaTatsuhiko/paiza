42314a4e5e415c10ef63f892d6f0605bbafd59ea
tatsupiko <identity2022@outlook.jp>
2023-10-06 13:37:19 +0900

    DPメニュー | 【漸化式】 3項間漸化式 2 | STEP1

---
+++ test0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int[] intArray = new int[data[2]];
+        intArray[0] = data[0];
+
+        for (int i = 1; i < data[2]; i++)
+        {
+            intArray[i] = intArray[i - 1] + data[1];
+        }
+        Console.WriteLine(intArray[data[2] - 1]);
+    }
+}
