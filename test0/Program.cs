cf23ae9f1c4b20b079a03a551dd7d5aa3a18b2fd
tatsupiko <identity2022@outlook.jp>
2023-10-06 13:39:46 +0900

    DPメニュー | 【漸化式】 3項間漸化式 2 | STEP4

--- test0/Program.cs
+++ test0/Program.cs
@@ -8,15 +8,24 @@ class Program
         int x = data[0];
         int d_1 = data[1];
         int d_2 = data[2];
-        int k = data[3];
+        int[] indeces = new int[int.Parse(Console.ReadLine())];
         int[] intArray = new int[1000 + 1];
         intArray[1] = x;
         int[] addValues = new int[] { d_2, d_1 };
+
+        for (int i = 0; i < indeces.Length; i++)
+        {
+            indeces[i] = int.Parse(Console.ReadLine());
+        }
 
         for (int i = 2, n = d_2; i <= 1000; i++, n = addValues[i % 2])
         {
             intArray[i] = intArray[i - 1] + n;
         }
-        Console.WriteLine(intArray[k]);
+
+        for (int i = 0; i < indeces.Length; i++)
+        {
+            Console.WriteLine(intArray[indeces[i]]);
+        }
     }
 }
