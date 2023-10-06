d0dd30d6f3666ff1abbe7c648397b52099f59355
tatsupiko <identity2022@outlook.jp>
2023-10-06 13:38:26 +0900

    DPメニュー | 【漸化式】 3項間漸化式 2 | STEP2

--- test0/Program.cs
+++ test0/Program.cs
@@ -5,13 +5,24 @@ class Program
     static void Main()
     {
         int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        int[] intArray = new int[data[2]];
-        intArray[0] = data[0];
+        int n = int.Parse(Console.ReadLine());
+        int[] intArray = new int[1000 + 1];
+        intArray[1] = data[0];
+        int[] indeces = new int[n];
 
-        for (int i = 1; i < data[2]; i++)
+        for (int i = 0; i < n; i++)
+        {
+            indeces[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 2; i <= 1000; i++)
         {
             intArray[i] = intArray[i - 1] + data[1];
         }
-        Console.WriteLine(intArray[data[2] - 1]);
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(intArray[indeces[i]]);
+        }
     }
 }
