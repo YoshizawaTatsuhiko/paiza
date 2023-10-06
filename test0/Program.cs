c03535bf011e2a552b774d547ae32865f94909b9
tatsupiko <identity2022@outlook.jp>
2023-10-06 13:41:39 +0900

    DPメニュー | 【漸化式】 3項間漸化式 2 | FINAL

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,15 +4,24 @@ class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
         int[] intArray = new int[40 + 1]; // 入力される値の最大値 + 1
         intArray[0] = 0;
         intArray[1] = 1;
+        int[] indeces = new int[int.Parse(Console.ReadLine())];
+
+        for (int i = 0; i < indeces.Length; i++)
+        {
+            indeces[i] = int.Parse(Console.ReadLine());
+        }
 
         for (int i = 2; i < intArray.Length; i++)
         {
             intArray[i] = intArray[i - 2] + intArray[i - 1];
         }
-        Console.WriteLine(intArray[n]);
+
+        foreach (var index in indeces)
+        {
+            Console.WriteLine(intArray[index]);
+        }
     }
 }
