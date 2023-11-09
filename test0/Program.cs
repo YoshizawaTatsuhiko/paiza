e4d860ac9e06b2eedf168cb3f6721cd3faea1a6f
tatsupiko <identity2022@outlook.jp>
2023-11-09 16:57:18 +0900

    線形探索メニュー応用編 | CSV ファイル 2 | STEP5

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine(string.Join(" ", Console.ReadLine().Split(',')));
+        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int n = data[0], x = data[1];
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] tmp = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+
+            if (tmp[1] >= x) Console.WriteLine($"{tmp[0]},{tmp[1]}");
+        }
     }
 }
