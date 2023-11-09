edce254d704c18a0ac61e532887715dfd28e3f61
tatsupiko <identity2022@outlook.jp>
2023-11-09 16:45:31 +0900

    線形探索メニュー応用編 | CSV ファイル 2 | STEP4

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,21 +4,6 @@ class Program
 {
     static void Main()
     {
-        string str = Console.ReadLine();
-        string subStr = Console.ReadLine();
-        Console.WriteLine(SubStringCount(str, subStr));
-    }
-
-    static int SubStringCount(string str, string subStr)
-    {
-        if (str.Length < subStr.Length) return 0;
-
-        int count = 0;
-
-        for (int i = 0; i <= str.Length - subStr.Length; i++)
-        {
-            if (str.Substring(i, subStr.Length) == subStr) count++;
-        }
-        return count;
+        Console.WriteLine(string.Join(" ", Console.ReadLine().Split(',')));
     }
 }
