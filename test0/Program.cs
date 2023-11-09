b1b80c16e4332d2b2bd4c6e56f77a4f441dc9aa8
tatsupiko <identity2022@outlook.jp>
2023-11-09 15:18:56 +0900

    線形探索メニュー応用編 | CSV ファイル 2 | STEP1

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,27 +4,12 @@ class Program
 {
     static void Main()
     {
-        string[] data = Console.ReadLine().Split();
-        int currentY = int.Parse(data[0]);
-        int currentX = int.Parse(data[1]);
-        string direction = data[2];
-        string order = Console.ReadLine();
+        string str = Console.ReadLine();
+        char c = Console.ReadLine()[0];
 
-        switch (direction)
+        for (int i = 0; i < str.Length; i++)
         {
-            case "N":
-                currentX += order == "R" ? 1 : -1;
-                break;
-            case "S":
-                currentX += order == "R" ? -1 : 1;
-                break;
-            case "E":
-                currentY += order == "R" ? 1 : -1;
-                break;
-            case "W":
-                currentY += order == "R" ? -1 : 1;
-                break;
+            if (str[i] == c) { Console.WriteLine(i + 1); break; }
         }
-        Console.WriteLine($"{currentY} {currentX}");
     }
 }
