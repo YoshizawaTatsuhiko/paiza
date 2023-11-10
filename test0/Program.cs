958a0bd2ee679471249e0cf0e405265e7ad26192
tatsupiko <identity2022@outlook.jp>
2023-11-10 16:15:06 +0900

    幅優先探索・深さ優先探索メニュー | 2 頂点間の距離 | STEP1

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,14 +4,38 @@ class Program
 {
     static void Main()
     {
-        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        int n = data[0], x = data[1];
+        int[] datas = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int h = datas[0], w = datas[1], y = datas[2], x = datas[3];
+        int[,] map = new int[w, h];
+        int walkCount = 3;
+        BFS(map, y, x, walkCount);
+    }
 
-        for (int i = 0; i < n; i++)
+    static Tuple<int, int>[] _dirs = new Tuple<int, int>[]
+    {
+        new Tuple<int, int>(-1, 0),
+        new Tuple<int, int>(0, 1),
+        new Tuple<int, int>(1, 0),
+        new Tuple<int, int>(0, -1),
+    };
+
+    static void BFS(int[,] map, int startY, int startX, int walkCount)
+    {
+        if (walkCount == 0)
         {
-            string line = Console.ReadLine();
+            Console.WriteLine($"{startY} {startX}");
+            return;
+        }
 
-            if (int.Parse(line.Split(',')[1]) >= x) Console.WriteLine(line);
+        foreach (var direction in _dirs)
+        {
+            int nextY = startY + direction.Item1;
+            int nextX = startX + direction.Item2;
+
+            if (0 <= nextY && nextY < map.GetLength(1) && 0 <= nextX && nextX < map.GetLength(0))
+            {
+                BFS(map, nextY, nextX, walkCount - 1);
+            }
         }
     }
 }
