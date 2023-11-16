9da46aed312b550be9215136070d46d4172ac3be
tatsupiko <identity2022@outlook.jp>
2023-11-16 17:11:24 +0900

    Aランクレベルアップメニュー | 陣取りゲーム | STEP1

--- test0/Program.cs
+++ test0/Program.cs
@@ -1,41 +1,49 @@
 ﻿using System;
+using System.Text;
 
 class Program
 {
     static void Main()
     {
-        int[] datas = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        int h = datas[0], w = datas[1], y = datas[2], x = datas[3];
-        int[,] map = new int[w, h];
-        int walkCount = 3;
-        BFS(map, y, x, walkCount);
-    }
-
-    static Tuple<int, int>[] _dirs = new Tuple<int, int>[]
-    {
-        new Tuple<int, int>(-1, 0),
-        new Tuple<int, int>(0, 1),
-        new Tuple<int, int>(1, 0),
-        new Tuple<int, int>(0, -1),
-    };
-
-    static void BFS(int[,] map, int startY, int startX, int walkCount)
-    {
-        if (walkCount == 0)
+        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int h = data[0], w = data[1];
+        StringBuilder[] map = new StringBuilder[h];
+        Tuple<int, int> myPos = null;
+        Tuple<int, int>[] searchDir =
         {
-            Console.WriteLine($"{startY} {startX}");
-            return;
-        }
+            new Tuple<int, int>(-1, +0), // UP
+            new Tuple<int, int>(+1, +0), // DOWN
+            new Tuple<int, int>(+0, -1), // LEFT
+            new Tuple<int, int>(+0, +1), // RIGHT
+        };
 
-        foreach (var direction in _dirs)
+        for (int r = 0; r < map.Length; r++)
         {
-            int nextY = startY + direction.Item1;
-            int nextX = startX + direction.Item2;
+            string str = Console.ReadLine();
+            map[r] = new StringBuilder(str);
 
-            if (0 <= nextY && nextY < map.GetLength(1) && 0 <= nextX && nextX < map.GetLength(0))
+            for (int c = 0; c < str.Length && myPos == null; c++)
             {
-                BFS(map, nextY, nextX, walkCount - 1);
+                if (str[c] == '*') myPos = new Tuple<int, int>(r, c);
             }
         }
+
+        foreach (var dir in searchDir)
+        {
+            int newRow = myPos.Item1 + dir.Item1;
+            int newCol = myPos.Item2 + dir.Item2;
+
+            if (IsIndexInRange(map, newRow, newCol))
+            {
+                map[newRow][newCol] = '*';
+            }
+        }
+
+        foreach (var line in map) Console.WriteLine(line);
+    }
+
+    static bool IsIndexInRange(StringBuilder[] map, int r, int c)
+    {
+        return 0 <= r && r < map.Length && 0 <= c && c < map[r].Length;
     }
 }
