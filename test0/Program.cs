b1d41d81f5f6743d41cf00be7a1e02019c75a21e
tatsupiko <identity2022@outlook.jp>
2023-11-19 22:13:40 +0900

    A幅優先探索・深さ優先探索メニュー | 連結成分の個数 | STEP1

--- test0/Program.cs
+++ test0/Program.cs
@@ -1,71 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 class Program
 {
-    private static HashSet<Coord> _myArea = new HashSet<Coord>();
-    private static Tuple<int, int>[] _searchDir =
-    {
-        new Tuple<int, int>(-1, +0), // UP
-        new Tuple<int, int>(+1, +0), // DOWN
-        new Tuple<int, int>(+0, -1), // LEFT
-        new Tuple<int, int>(+0, +1), // RIGHT
-    };
+    private static Dictionary<int, List<int>> _adjacencyList = new Dictionary<int, List<int>>();
+    private static Stack<int> _stack = new Stack<int>();
+    private static HashSet<int> _visitedList = new HashSet<int>();
 
-    static void Main()
+    private static void Main()
     {
-        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        int h = data[0], w = data[1];
-        StringBuilder[] map = new StringBuilder[h];
-        Tuple<int, int> myPos = null;
+        int[] datas = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int n = datas[0], m = datas[1], x = datas[2];
 
-        for (int r = 0; r < map.Length; r++)
+        for (int i = 1; i <= n; i++) _adjacencyList.Add(i, new List<int>());
+
+        for (int i = 0; i < m; i++)
         {
-            string str = Console.ReadLine();
-            map[r] = new StringBuilder(str);
-
-            for (int c = 0; c < str.Length && myPos == null; c++)
-            {
-                if (str[c] == '*') myPos = new Tuple<int, int>(r, c);
-            }
+            int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            _adjacencyList[data[0]].Add(data[1]);
+            _adjacencyList[data[1]].Add(data[0]);
         }
-        Invade(map, myPos.Item1, myPos.Item2);
 
-        foreach (var line in map) Console.WriteLine(line);
+        foreach(var valueList in _adjacencyList.Values) valueList.Sort();
+        DFS(x);
+
+        foreach(var item in _visitedList) Console.WriteLine(item);
     }
 
-    static bool IsIndexInRange(StringBuilder[] map, int r, int c)
+    private static void DFS(int vertex)
     {
-        return 0 <= r && r < map.Length && 0 <= c && c < map[r].Length;
-    }
+        if (!_visitedList.Add(vertex)) return;
 
-    static void Invade(StringBuilder[] map, int startRow, int startCol)
-    {
-        if (!IsIndexInRange(map, startRow, startCol)) return;
-        if (_myArea.Contains(new Coord(startRow, startCol))) return;
-        if (map[startRow][startCol] == '#') return;
-
-        map[startRow][startCol] = '*';
-        _myArea.Add(new Coord(startRow, startCol));
-
-        foreach (var dir in _searchDir)
+        foreach (var data in _adjacencyList[vertex])
         {
-            int nextRow = startRow + dir.Item1;
-            int nextCol = startCol + dir.Item2;
-            Invade(map, nextRow, nextCol);
+            _stack.Push(vertex);
+            DFS(data);
+            _stack.Pop();
         }
-    }
-}
-
-struct Coord
-{
-    public int Y;
-    public int X;
-
-    public Coord(int y, int x)
-    {
-        Y = y;
-        X = x;
     }
 }
