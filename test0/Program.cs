aa04860d454294507f649ac353129f4177235456
tatsupiko <identity2022@outlook.jp>
2023-12-08 16:02:38 +0900

    ユークリッドの互除法メニュー | 意地悪すごろく | STEP1

--- test0/Program.cs
+++ test0/Program.cs
@@ -1,21 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
 
 class Program
 {
     private static void Main()
     {
-        int[] intArray = null;
+        string[] datas = Console.ReadLine().Split();
+        int a = int.Parse(datas[0]);
+        int b = int.Parse(datas[1]);
+        string cal = datas[2];
+        int c = int.Parse(datas[3]);
+        int d = int.Parse(datas[4]);
+        int numerator = 0;
+        int denominator = 0;
 
-        Console.ReadLine();
-        intArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-        for (int i = 0, loopCount = int.Parse(Console.ReadLine()); i < loopCount; i++)
+        switch (cal)
         {
-            int data = int.Parse(Console.ReadLine());
-            int n = Array.BinarySearch(intArray, data);
-
-            Console.WriteLine(n < 0 ? "No" : "Yes");
+            case "+":
+                numerator = a * d + c * b;
+                denominator = b * d;
+                break;
+            case "-":
+                numerator = a * d - c * b;
+                denominator = b * d;
+                break;
+            case "*":
+                numerator = a * c;
+                denominator = b * d;
+                break;
+            case "/":
+                numerator = a * d;
+                denominator = b * c;
+                break;
         }
+        var ans = Simplify(numerator, denominator);
+
+        if (ans.Item2 < 0) Console.WriteLine($"{-ans.Item1} {-ans.Item2}");
+        else Console.WriteLine($"{ans.Item1} {ans.Item2}");
+    }
+
+    private static int GCD(int a, int b) => b == 0 ? a : GCD(b, a % b);
+
+    private static Tuple<int, int> Simplify(int n, int d)
+    {
+        int gcd = GCD(n, d);
+        if(gcd == 1) return Tuple.Create(n, d);
+
+        n /= gcd;
+        d /= gcd;
+        return Simplify(n, d);
     }
 }
