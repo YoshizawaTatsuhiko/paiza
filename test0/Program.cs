031b1cdab2fc924943db7644d6ca34b0dfb98282
tatsupiko <identity2022@outlook.jp>
2023-12-15 15:45:44 +0900

    ユークリッドの互除法メニュー応用編 | 本棚 | STEP3

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,8 +4,36 @@ class Program
 {
     private static void Main()
     {
-        long[] datas = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-        Console.WriteLine(LCM(datas[0], datas[1]));
+        long n = long.Parse(Console.ReadLine());
+        long[] data = new long[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            data[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"{GCD(data)}\n{LCM(data)}");
+    }
+
+    private static long GCD(long[] datas)
+    {
+        long gcd = datas[0];
+
+        for (int i = 1; i < datas.Length; i++)
+        {
+            gcd = GCD(gcd, datas[i]);
+        }
+        return gcd;
+    }
+
+    private static long LCM(long[] datas)
+    {
+        long lcm = datas[0];
+
+        for (int i = 1; i < datas.Length; i++)
+        {
+            lcm = LCM(lcm, datas[i]);
+        }
+        return lcm;
     }
 
     private static long GCD(long a, long b) => b == 0 ? a : GCD(b, a % b);
