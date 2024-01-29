68b401fe066250e27f6c4d37ee59dbfee9a388a8
tatsupiko <identity2022@outlook.jp>
2024-01-29 16:54:16 +0900

    スキルチェック問題 | B138 | ドーナツ

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,15 +4,53 @@ class Program
 {
     private static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
-        long[] fiboArray = new long[n + 1];
-        fiboArray[0] = 0;
-        fiboArray[1] = 1;
+        int[] data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int h = data[0], w = data[1];
+        string[] map = new string[h];
 
-        for (int i = 2; i <= n; i++)
+        for (int i = 0; i < h; i++)
         {
-            fiboArray[i] = (fiboArray[i - 1] + fiboArray[i - 2]) % 1000000007;
+            map[i] = Console.ReadLine();
         }
-        Console.WriteLine(fiboArray[fiboArray.Length - 1]);
+        Console.WriteLine(DonutsCounter(map));
+    }
+
+    private static bool IsInRange(string[] map, int r, int c)
+    {
+        return 0 <= r && r < map.Length && 0 <= c && c < map[r].Length;
+    }
+
+    private static int DonutsCounter(string[] map)
+    {
+        Tuple<int, int>[] dirs = {
+            new Tuple<int, int>(-1, -1),
+            new Tuple<int, int>(-1, 0),
+            new Tuple<int, int>(-1, 1),
+            new Tuple<int, int>(0, -1),
+            new Tuple<int, int>(0, 1),
+            new Tuple<int, int>(1, -1),
+            new Tuple<int, int>(1, 0),
+            new Tuple<int, int>(1, 1),
+        };
+        int donutsCount = 0;
+
+        for (int r = 1, count = 0; r < map.Length - 1; r++)
+        {
+            for (int c = 1; c < map[r].Length - 1; c++, count = 0)
+            {
+                if (map[r][c] != '.') continue;
+
+                foreach (var dir in dirs)
+                {
+                    if (!IsInRange(map, r + dir.Item1, c + dir.Item2)) break;
+                    if (map[r + dir.Item1][c + dir.Item2] != '#') break;
+
+                    count++;
+                }
+
+                if (count == dirs.Length) donutsCount++;
+            }
+        }
+        return donutsCount;
     }
 }
